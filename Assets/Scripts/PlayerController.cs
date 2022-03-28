e9375697609f1094cc77282e2e6b40655e12f0bd
jason <jkoo2088@gmail.com>
2022-03-28 18:00:31 -0400

    Extend map

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,8 +20,6 @@ public class PlayerController : MonoBehaviour
     [HideInInspector] 
     public bool playerActive;
 
-    public AudioSource footStep;
-    public AudioSource waterBucket;
 
     private void Awake()
     {
@@ -45,22 +43,18 @@ public class PlayerController : MonoBehaviour
 
         if(fireTilemap.GetTile(positionRight)){
             fireTilemap.SetTile(positionRight, null);
-            waterBucket.Play();
             activeFires -= 1;
         }
         else if(fireTilemap.GetTile(positionLeft)){
             fireTilemap.SetTile(positionLeft, null);
-            waterBucket.Play();
             activeFires -= 1;
         }
         else if(fireTilemap.GetTile(positionDown)){
             fireTilemap.SetTile(positionDown, null);
-            waterBucket.Play();
             activeFires -= 1;
         }
         else if(fireTilemap.GetTile(positionUp)){
             fireTilemap.SetTile(positionUp, null);
-            waterBucket.Play();
             activeFires -= 1;
         }
     }
@@ -85,7 +79,6 @@ public class PlayerController : MonoBehaviour
     {
         if(CanMove(direction)){
             transform.position += (Vector3)direction;
-            footStep.Play();
             playerActive = true;
         }
     }
