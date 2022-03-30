03ac56503157f6eed67906410089ac31b4bf8cdc
Ewhan Han <ewhan.han@gmail.com>
2022-03-29 20:48:27 -0400

    Zoom in camera and zoom out minimap + Fix jitter

--- Assets/MinimapController.cs
+++ Assets/MinimapController.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 public class MinimapController : MonoBehaviour
 {
   public Transform player;
+  public float cameraSpeed = 5f;
 
-  private void LateUpdate()
+  private void FixedUpdate()
   {
     Vector3 playerPos = player.position;
     playerPos.z = transform.position.z;
-    transform.position = new Vector3((float) Math.Round(playerPos.x), (float) Math.Round(playerPos.y),
+    var _newCamPos = new Vector3((float) Math.Round(playerPos.x), (float) Math.Round(playerPos.y),
       (float) Math.Round(playerPos.z));
+
+    transform.position = Vector3.Lerp(transform.position, _newCamPos, cameraSpeed * Time.deltaTime);
   }
 }
