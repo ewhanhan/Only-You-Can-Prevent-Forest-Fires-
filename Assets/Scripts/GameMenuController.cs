46bdf3c58450b5bc68b8a56ba311bc88b3f811db
Ewhan <40779221+EwhanHan@users.noreply.github.com>
2022-03-31 12:16:19 -0400

    Fix(build)-Remove EditorApplication in GameMenuController

--- Assets/Scripts/GameMenuController.cs
+++ Assets/Scripts/GameMenuController.cs
@@ -106,10 +106,6 @@ public class GameMenuController : MonoBehaviour
   public void QuitGame()
   {
     Application.Quit();
-    if (EditorApplication.isPlaying)
-    {
-      EditorApplication.ExitPlaymode();
-    }
   }
 
   public void Restart()
